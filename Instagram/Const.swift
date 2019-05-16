019320338a8d1ac88b0c112507cebb857f6ddf53
関口 咲季 <s-sekiguchi@i-enter.co.jp>
2019-05-16 10:33:03 +0900

    諸々

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 関口 咲季 on 2019/05/15.
+//  Copyright © 2019 saki.sekiguchi. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
